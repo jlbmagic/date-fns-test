7a012657c08c4e19bf24d3dad50b78d294d6dc38
Jeremy Brown <jeremy.brown@proofgeist.com>
2021-09-23 19:08:36 -0400

    tested date-fns and added two functions

--- public/index.js
+++ public/index.js
@@ -1,3 +1,38 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import "regenerator-runtime/runtime.js";
+import {
+  format,
+  compareAsc,
+  subDays,
+  formatDistance,
+  nextSaturday,
+  previousFriday,
+} from "date-fns";
+
+window.getNextSaturday = function (json) {
+  const data = JSON.parse(json);
+
+  const fmDate = data.fmDate;
+  const fmDateDate = new Date(fmDate);
+
+  const nextSat = format(nextSaturday(fmDateDate), "MM/dd/yyyy");
+  console.log(nextSat);
+
+  const obj = { nextSat };
+  console.log(obj);
+  FileMaker.PerformScript("Get Result", JSON.stringify(obj));
+};
+
+window.getPreviousFriday = function (json) {
+  const data = JSON.parse(json);
+
+  const fmDate = data.fmDate;
+  const fmDateDate = new Date(fmDate);
+
+  const prevFriday = format(previousFriday(fmDateDate), "MM/dd/yyyy");
+
+  const obj = { prevFriday };
+  console.log(obj);
+  FileMaker.PerformScript("Get Result", JSON.stringify(obj));
+};
